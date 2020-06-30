77e0aac5284098c3af4b51c132c6cd6cdcfe3612
Tye <darthtye@gmail.com>
2020-06-30 04:37:49 -0400

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './header.scss'
+import {animateScroll as scroll} from 'react-scroll'
+
+const Header = () => {
+	return (
+		<header className="c-header">
+			<div className="c-header__logo" onClick={() => scroll.scrollToTop()}></div>
+			<button className="c-header__burger c-header__burger--open"></button>
+		</header>
+	)
+}
+
+export default Header
---
+++ src/components/Main/Main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './main.scss'
+import arrowDown from '../../images/arrow-down.svg'
+import {Link} from 'react-scroll'
+
+const Main = () => {
+	return (
+		<main className="c-main">
+			<div className="l-container">
+				<div className="c-main__content">
+					<h1>Hi there.</h1>
+					<p>I'm one of the most versatile developers on the 🌏</p>
+					<Link className="c-main__contact" to="contact" smooth={true} duration={1000}>Contact</Link>
+				</div>
+				<img className="arrow-down" src={arrowDown} alt=""/>
+			</div>
+		</main>
+	)
+}
+
+export default Main
